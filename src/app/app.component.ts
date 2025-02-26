fc01e8c5d942943e71637461d6081add496dbf6e
ThengaraiMaharajan <tmaharajan1999@gmail.com>
2025-02-26 17:29:22 +0530

    removed animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,34 +11,17 @@ import FingerprintJS from '@fingerprintjs/fingerprintjs';
 export class AppComponent implements OnInit {
 
   isSidebarVisible = true;
-  isUserLoggedIn : boolean = false;
-
-  backgroundAnimationNo!:number;
 
   constructor(
     private analytics : AnalyticsService
   ){}
 
   ngOnInit(): void {
-    
-    this.generateFingerPrint();
-    this.backgroundAnimationNo = Math.floor(Math.random() * 6) + 1;
 
-  }
-
-  async generateFingerPrint(){
-    const fp = await FingerprintJS.load();
-    const result = await fp.get();
-    const fingerprint = result.visitorId;
-    this.analytics.setAnalytics('Application Loaded','Initialization','User View your website',`${fingerprint+'-Viewed your website'}`,fingerprint);
   }
 
   toggleSidebar() {
     this.isSidebarVisible = !this.isSidebarVisible;
-  }
-  
-  animationStyleChange(animationID : number){
-    this.backgroundAnimationNo = animationID;
   }
 
 }
