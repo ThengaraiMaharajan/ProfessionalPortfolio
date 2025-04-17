5061e979dc781bb0df623bac171462442618148e
ThengaraiMaharajanSubbiah <tmaharajan1999@gmail.com>
2025-04-17 15:08:42 +0530

    code commit

--- src/app/modules/professional-portfolio/components/profile/profile.component.ts
+++ src/app/modules/professional-portfolio/components/profile/profile.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { externalLink } from '../../../../../enums/externalLinks.enum';
-import { environment } from '../../../../../environments/environment';
 
 @Component({
   selector: 'app-profile',
